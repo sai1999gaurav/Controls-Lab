a6f61ea8ca4776e4654537b349506b3ae1724b38
jayeshchoudhary <jayeshchoudhary2000@gmail.com>
2020-01-31 15:17:15 +0530

    Update sketch_jan31a.ino
    calibration of IR sensor

--- Line Follower/sketch_jan31a/sketch_jan31a.ino
+++ Line Follower/sketch_jan31a/sketch_jan31a.ino
@@ -1,72 +1,73 @@
-float Kp = 50;   // Max deviation = 8-4.5 = 3.5 ||  255/3.5 = 72
-float Ki = 0.1;
-float Kd = 3;
-int potvalue, initangle, targetangle;
-//#include <PID_v1.h>
-#define potoutput A10
-const int motorpin1 = 9;
-const int motorpin2 = 10;
-const int enable = 6;
-float error = 0;
-float previousError = 0;
-float totalError = 0;
-float power = 0;
-float avgSensor;
-int highval =0;
+/*
+  Calibration
 
-void PID_program()
-{ 
-    //readSensor(); 
-    previousError = error; // save previous error for differential 
-    error = targetangle - potvalue; // Count how much robot deviate from center
-    totalError += error; // Accumulate error for integral
-    
-    power = (Kp*error) + (Kd*(error-previousError)) + (Ki*totalError);
-    //Serial.println(power);
-    highval = 45 + abs(power*190/(Kp*500));  //65-255
-    if(highval>255)
-    highval = 255;
-    Serial.println(highval);
-    if(power>0) // Turn left
-    {  
-        analogWrite(motorpin1, highval); //high
-        digitalWrite(motorpin2, LOW);
-    }
-    else // Turn right
-    {
-      analogWrite(motorpin2, highval);
-      digitalWrite(motorpin1, LOW);
-    }
-}
+  Demonstrates one technique for calibrating sensor input. The sensor readings
+  during the first five seconds of the sketch execution define the minimum and
+  maximum of expected values attached to the sensor pin.
+
+  The sensor minimum and maximum initial values may seem backwards. Initially,
+  you set the minimum high and listen for anything lower, saving it as the new
+  minimum. Likewise, you set the maximum low and listen for anything higher as
+  the new maximum.
+
+  The circuit:
+  - analog sensor (potentiometer will do) attached to analog input 0
+  - LED attached from digital pin 9 to ground
+
+  created 29 Oct 2008
+  by David A Mellis
+  modified 30 Aug 2011
+  by Tom Igoe
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/Calibration
+*/
+
+// These constants won't change:
+const int sensorPin = A0;    // pin that the sensor is attached to
+const int ledPin = 9;        // pin that the LED is attached to
+
+// variables:
+int sensorValue = 0;         // the sensor value
+int sensorMin = 1023;        // minimum sensor value
+int sensorMax = 0;           // maximum sensor value
+
 
 void setup() {
- Serial.begin(9600);
- pinMode(potoutput, INPUT);  // 0 to 1023//  --- (v + 512)%1023
- pinMode(enable, OUTPUT);
- pinMode(motorpin1, OUTPUT);
- pinMode(motorpin2, OUTPUT);
- digitalWrite(enable, HIGH);
- initangle = analogRead(potoutput);
- targetangle = (initangle + 512)%1023;
- Serial.print("Iangle: ");
- Serial.println(initangle);
- Serial.print("tangle: ");
- Serial.println(targetangle);
+  // turn on LED to signal the start of the calibration period:
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
 
-  // put your setup code here, to run once:
+  // calibrate during the first five seconds
+  while (millis() < 5000) {
+    sensorValue = analogRead(sensorPin);
 
+    // record the maximum sensor value
+    if (sensorValue > sensorMax) {
+      sensorMax = sensorValue;
+    }
+
+    // record the minimum sensor value
+    if (sensorValue < sensorMin) {
+      sensorMin = sensorValue;
+    }
+  }
+
+  // signal the end of the calibration period
+  digitalWrite(13, LOW);
 }
 
 void loop() {
-  delay(10);
-  potvalue = analogRead(potoutput);
-  Serial.print("Angle: ");
-  Serial.println(potvalue);
-  PID_program();
-  //analogWrite(motorpin1, 65);
-  //analogWrite(motorpin2, 0);
+  // read the sensor:
+  sensorValue = analogRead(sensorPin);
 
-  
-  // put your main code here, to run repeatedly:
+  // apply the calibration to the sensor reading
+  sensorValue = map(sensorValue, sensorMin, sensorMax, 0, 255);
 
+  // in case the sensor value is outside the range seen during calibration
+  sensorValue = constrain(sensorValue, 0, 255);
+
+  // fade the LED using the calibrated value:
+  analogWrite(ledPin, sensorValue);
 }
